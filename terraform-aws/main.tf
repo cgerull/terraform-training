533e514d70e8332d3ab5c48a396faac29e252567
Claus Gerull <cgerull@live.com>
2021-09-13 12:33:18 +0200

    Remove stale code.

--- terraform-aws/main.tf
+++ terraform-aws/main.tf
@@ -54,23 +54,6 @@ resource "aws_security_group" "alb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-# resource "aws_instance" "web_server" {
-#   ami                     = var.ubuntu_ami_amd64
-#   instance_type           = "t2.micro"
-#   vpc_security_group_ids  = [aws_security_group.sg_web_public.id]
-
-#   user_data = <<-EOF
-#               #!/bin/bash
-#               echo "Hello, World from AWS" > index.html
-#               nohup busybox httpd -f -p ${var.private_http_port} &
-#               EOF
-
-#   tags = {
-#     owner       = "claus"
-#     name        = "terraform-example"
-#     environment = "training"
-#   }
-# }
 
 resource "aws_launch_configuration" "web_server" {
   image_id                = var.ubuntu_ami_amd64
