cc49a905bce96f2e8beb2f3c55cdea749379a056
Claus Gerull <cgerull@live.com>
2021-09-13 20:41:53 +0200

    Add deactived template.

--- stage/services/webservices/main.tf
+++ stage/services/webservices/main.tf
@@ -75,12 +75,15 @@ resource "aws_launch_configuration" "web_server" {
   image_id        = var.ubuntu_ami_amd64
   instance_type   = "t2.micro"
   security_groups = [aws_security_group.sg_web_public.id]
-
+  # user_data       = data.template_file.user_data.rendered
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hello, World from AWS" > index.html
+              echo "Hello, World from AWS<br>" >> index.html
+              echo "Database server: ${data.terraform_remote_state.db.outputs.address}" >> index.html
+              echo "${data.terraform_remote_state.db.outputs.port}<br>" >> index.html
               nohup busybox httpd -f -p ${var.private_http_port} &
               EOF
+
   lifecycle {
     create_before_destroy = true
   }
